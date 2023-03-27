b5650623478de3e098e1b20621cc9a641704d962
heisenberg550 <150204095@kku.edu.tr>
2023-03-28 02:26:10 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(arr, name){
+    const x = arr.filter(x => x.toUpperCase() === name.toUpperCase())
+    return x
+}
+function fuzzyMatch(arr, letter){
+    const name = arr.filter(x => x.startsWith(letter))
+    return name ? name : undefined; 
+}
+
+function matchName(arr, name){
+ const element = arr.filter(x => x.name === name)
+ return element
+}
